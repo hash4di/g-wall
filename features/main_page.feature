4fe53b3e1137060b5be92ed31c0136d323a37835
hash4di <hash4di@doubledrones.com>
2011-04-29 13:58:32 +0200

    add imageg and description

--- features/main_page.feature
+++ features/main_page.feature
@@ -7,12 +7,11 @@ Feature: Main Page
 
     And I should see G-wall Name
 
-    And I should have Comics Story Image
+    And I should see Comics Story Image
 
-    And I should have Script Description
+    And I should see Script Description "Example Script Description"
     And I should have Script Area
 
     And I should have Download link
 
     And I should see Footer
-
--- features/step_definitions/main_page.rb
+++ features/step_definitions/main_page.rb
@@ -6,3 +6,12 @@ Then /^I should see G\-wall Name$/ do
   page.should have_xpath("///div[@id='header']/div[@id='name']/span[contains(text(), \"G-wall\")]")
 end
 
+Then /^I should see Comics Story Image$/ do
+  page.should have_xpath("//div[@id='content']/div[@id='comics_story']/img[@id='comics']")
+end
+
+Then /^I should see Script Description "([^"]*)"$/ do |text|
+  page.should have_xpath("//div[@id='content']/div[@id='description'][contains(text(), \"#{text}\")]")
+end
+
+
