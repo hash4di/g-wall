3cb311799c60d44e0ec9032e54bd3c27c453e765
hash4di <hash4di@doubledrones.com>
2011-04-29 13:29:19 +0200

    create  website name

--- features/step_definitions/main_page.rb
+++ features/step_definitions/main_page.rb
@@ -2,3 +2,7 @@ Then /^I should see Header$/ do
   page.should have_xpath("//div[@id='header']")
 end
 
+Then /^I should see G\-wall Name$/ do
+  page.should have_xpath("///div[@id='header']/div[@id='name']/span[contains(text(), \"G-wall\")]")
+end
+
