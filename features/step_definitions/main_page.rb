2345ba6842f8062b997567b68fde4f454dcc2a9a
hash4di <hash4di@doubledrones.com>
2011-05-04 13:15:38 +0200

    add initial view and sweet style

--- features/step_definitions/main_page.rb
+++ features/step_definitions/main_page.rb
@@ -3,15 +3,15 @@ Then /^I should see Header$/ do
 end
 
 Then /^I should see G\-wall Name$/ do
-  page.should have_xpath("///div[@id='header']/div[@id='name']/span[contains(text(), \"G-wall\")]")
+  page.should have_xpath("///div[@id='header']/div[@id='name']")
+end
+
+Then /^I should see Script Description "([^"]*)"$/ do |text|
+  page.should have_xpath("//div[@id='content']/div[@id='description']")
 end
 
 Then /^I should see Comics Story Image$/ do
   page.should have_xpath("//div[@id='content']/div[@id='comics_story']/img[@id='comics']")
-end
-
-Then /^I should see Script Description "([^"]*)"$/ do |text|
-  page.should have_xpath("//div[@id='content']/div[@id='description'][contains(text(), \"#{text}\")]")
 end
 
 Then /^I should have Script Area$/ do
